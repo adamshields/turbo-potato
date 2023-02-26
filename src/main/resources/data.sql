84697f87b1767d56a30b0a27eb0502425665f929
Adam Shields <adamenote@gmail.com>
2023-02-26 14:55:23 -0500

    I have h2 local working and dev working I need to implement liquibase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,3 @@
-@Component
-public class TestDataSeeder {
-
-    private final DesignRepository designRepository;
-
-    @Autowired
-    public TestDataSeeder(DesignRepository designRepository) {
-        this.designRepository = designRepository;
-    }
-
-    @PostConstruct
-    public void seedData() {
-        List<DesignModel> designs = new ArrayList<>();
-
-        // create some test data
-        DesignModel design1 = new DesignModel();
-        design1.setName("Design 1");
-        design1.setDescription("This is the first design.");
-        design1.setIsApproved(false);
-        designs.add(design1);
-
-        DesignModel design2 = new DesignModel();
-        design2.setName("Design 2");
-        design2.setDescription("This is the second design.");
-        design2.setIsApproved(false);
-        designs.add(design2);
-
-        // save the test data to the database
-        designRepository.saveAll(designs);
-    }
-}
+INSERT INTO CITY VALUES (11, 'Delhi', 110001);
+INSERT INTO CITY VALUES (12, 'Kanpur', 208001);
+INSERT INTO CITY VALUES (13, 'Lucknow', 226001);
