0bbea6f19ea5e535c1d301feecff742e54c30212
Adam Shields <adamenote@gmail.com>
2025-04-20 12:11:00 -0400

    setup main for moox practice

--- src/main/java/com/example/adam/javapractice/Sandbox.java
+++ src/main/java/com/example/adam/javapractice/Sandbox.java
@@ -1,6 +1,6 @@
 package com.example.adam.javapractice;
 
-public class MoocMain {
+public class Sandbox {
     public static void main(String[] args ) {
         System.out.println("Hello World");
     }
